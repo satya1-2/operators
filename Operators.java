6ba34cc488d90699ed53601f8bd9e943bfc8900f
satya <satya143@gmail.com>
2023-02-02 14:03:06 +0530

    all operators

---
+++ Operators.java
@@ -0,0 +1,70 @@
+package com.company;
+
+public class Operators {
+    public static void main(String[] args) {
+        // airthmatic operation
+       /* System.out.println("calculate operators :");
+        int a=4;
+        int b=6%a;;
+        System.out.println(b);*/
+
+        //assignment operatators=,+=,-= etc
+      /*   int b=9;
+         b *=3;
+        System.out.println(b);*/
+
+        // comparision operators ==,>=,>=
+        /*System.out.println(6==8);
+        System.out.println(64>=6);*/
+
+        //logical operator &&,||, !
+        /*System.out.println(64>5 && 64>98);
+        System.out.println(64>5 || 64>8);*/
+
+        //bitwise operators &,| work won bits
+        /*System.out.println(2&3);
+        System.out.println(2|3);*/
+
+        //trisary oerators
+        int num1 =12;
+        int num2=10;
+        int res=(num1> num2)? (num1 + num2) : (num1-num2);
+        System.out.println(res);
+
+        // increment decrement ++ --
+      /*  int y=7;
+       // int x=y++*8;
+        //int x=++y*8;
+        System.out.println(x);*/
+
+       /* char a='b';
+        a++;
+        System.out.println(a);*/
+
+        // prescedence
+      // int a=6*5-34/2;
+        //System.out.println(a);
+      // int b=6/5-34*2;
+       // System.out.println(b);
+
+        //quiz
+        /*int x=5;
+        int y=1;
+        int k=x *y/2;
+        System.out.println(k);*/
+
+        /*int b=1;
+        int c=4;
+        int a=5;
+        int k=(b*b-4*a*c)/(2*a);
+        System.out.println(k);*/
+
+        /*int u=1;
+        int v=5;
+        int s=1;
+                int z;
+        z=(u*u-v*v)/(4*u*s);
+        System.out.println(z);*/
+    }
+
+}
